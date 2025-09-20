f2fc17d215b29d786d3a0aa1cecee1c58b26242b
Thomas Fach-Pedersen <thomas@fach-pedersen.net>
2025-09-20 20:55:19 +0200

    SAVEGAME: Read in sietches

--- crates/savegame/src/bin/display_savegame.rs
+++ crates/savegame/src/bin/display_savegame.rs
@@ -1,7 +1,10 @@
 use std::{env, fs, io::Cursor};
 
 use bin_read::BinRead;
-use savegame::{data::Save, decompress_sav};
+use savegame::{
+    data::{Save, Sietch},
+    decompress_sav,
+};
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -26,9 +29,80 @@ fn main() -> std::io::Result<()> {
     let mut r = Cursor::new(&unparsed_savegame.data);
     let data = Save::bin_read(&mut r).unwrap();
 
-    println!("{:#?}", &data.data_segment);
+    let ds = &data.data_segment;
+    println!("rand_bits: {:04x}", ds.rand_bits);
+    println!("game_time: {:04x}", ds.game_time);
+    println!(
+        "current_location_and_room: {:04x}",
+        ds.current_location_and_room
+    );
 
-    println!("gametime: {:04x}", data.data_segment.game_time);
+    for i in 0..70 {
+        display_sietch(&ds.sietches[i]);
+    }
 
     Ok(())
+}
+
+fn display_sietch(s: &Sietch) {
+    let first_names = [
+        "Arrakeen", "Carthag", "Tuono", "Habbanya", "Oxtyn", "Tsympo", "Bledan", "Ergsun", "Haga",
+        "Cielago", "Sihaya", "Celimyn",
+    ];
+    let last_names = [
+        "(Atreides)",
+        "(Harkonnen)",
+        "Tabr",
+        "Timin",
+        "Tuek",
+        "Harg",
+        "Clam",
+        "Tsymyn",
+        "Siet",
+        "Pyons",
+        "Pyort",
+    ];
+
+    let name = format!(
+        "{}{}{}",
+        first_names
+            .get((s.first_name - 1) as usize)
+            .cloned()
+            .unwrap_or_default(),
+        if s.last_name < 3 { ' ' } else { '-' },
+        last_names
+            .get((s.last_name - 1) as usize)
+            .cloned()
+            .unwrap_or_default()
+    );
+
+    println!("{name}");
+    println!("====================");
+    println!("               desert: {}", s.desert);
+    println!("                map_x: {}", s.map_x);
+    println!("                map_y: {}", s.map_y);
+    println!("                map_u: {}", s.map_u);
+    println!("            another_x: {}", s.another_x);
+    println!("            another_y: {}", s.another_y);
+    println!("            apparence: {}", s.apparence);
+    println!("             troop_id: {}", s.troop_id);
+    println!("               status: {}", s.status);
+    println!("discoverable_at_phase: {}", s.discoverable_at_phase);
+    println!("                 unk1: {}", s.unk1);
+    println!("                 unk2: {}", s.unk2);
+    println!("                 unk3: {}", s.unk3);
+    println!("                 unk4: {}", s.unk4);
+    println!("       spice_field_id: {}", s.spice_field_id);
+    println!("                 unk5: {}", s.unk5);
+    println!("        spice_density: {}", s.spice_density);
+    println!("                 unk6: {}", s.unk6);
+    println!("            nbr_moiss: {}", s.nbr_moiss);
+    println!("             nbr_orni: {}", s.nbr_orni);
+    println!("            nbr_knife: {}", s.nbr_knife);
+    println!("             nbr_guns: {}", s.nbr_guns);
+    println!("             nbr_mods: {}", s.nbr_mods);
+    println!("            nbr_atoms: {}", s.nbr_atoms);
+    println!("            nbr_bulbs: {}", s.nbr_bulbs);
+    println!("                water: {}", s.water);
+    println!();
 }
--- crates/savegame/src/data.rs
+++ crates/savegame/src/data.rs
@@ -2,10 +2,42 @@ use bin_read::BinRead;
 
 #[derive(BinRead, Debug)]
 pub struct Save {
-    pub map_data: [u8; 50684 / 4],
-    pub unknown: [u8; 162],
-    pub dialogue: [u8; 4600],
+    pub map_data: [u8; 0x317f],
+    pub unknown: [u8; 0xa2],
+    pub dialogue: [u8; 0x11f8],
     pub data_segment: DataSegment,
+}
+
+#[derive(BinRead, Debug)]
+pub struct Sietch {
+    pub first_name: u8,
+    pub last_name: u8,
+    pub desert: u8,
+    pub map_x: u8,
+    pub map_y: u8,
+    pub map_u: u8,
+    pub another_x: u8,
+    pub another_y: u8,
+    pub apparence: u8,
+    pub troop_id: u8,
+    pub status: u8,
+    pub discoverable_at_phase: u8,
+    pub unk1: u8,
+    pub unk2: u8,
+    pub unk3: u8,
+    pub unk4: u8,
+    pub spice_field_id: u8,
+    pub unk5: u8,
+    pub spice_density: u8,
+    pub unk6: u8,
+    pub nbr_moiss: u8,
+    pub nbr_orni: u8,
+    pub nbr_knife: u8,
+    pub nbr_guns: u8,
+    pub nbr_mods: u8,
+    pub nbr_atoms: u8,
+    pub nbr_bulbs: u8,
+    pub water: u8,
 }
 
 #[derive(BinRead, Debug)]
@@ -27,6 +59,9 @@ pub struct DataSegment {
 
     #[bin_read(offset = 0x00e8)]
     pub ui_head_index: u8,
+
+    #[bin_read(offset = 0x0100)]
+    pub sietches: [Sietch; 70],
 
     #[bin_read(offset = 0x11dd)]
     pub intro_scene_28_attack_sprite_list: UISpriteList<2>,
